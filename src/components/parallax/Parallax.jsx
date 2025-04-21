e2000a969d9552e7658c142fb8af5b1369a55d99
CastroNicolas <maximonicolascastro@gmail.com>
2025-04-21 02:03:23 +0200

    pictures modify

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -44,7 +44,7 @@ export const Parallax = ({ type }) => {
                 className="planets"
                 style={{
                     y: yBg,
-                    backgroundImage: `url(${showMoon.includes(type) ? "../../../public/imgStyles/planets.png" : "../../../public/imgStyles/sun.png"})`,
+                    backgroundImage: `url(${showMoon.includes(type) ? "/imgStyles/planets.png" : "/imgStyles/sun.png"})`,
                 }}
             ></motion.div>
             <motion.div style={{ x: yBg }} className="stars"></motion.div>
